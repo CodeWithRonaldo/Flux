07466a9a04a093c52b23a5e0c76fdca6c4abd40b
olamilekan5162 <opeyemiolalekan481@gmail.com>
2026-02-13 01:12:06 +0100

    Add MusicCard component and integrate with Home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -2,11 +2,82 @@ import React from "react";
 import styles from "./Home.module.css";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import Player from "../../assets/player.png";
+import MusicCard from "../../components/MusicCard/MusicCard";
+import Music1 from "../../assets/fakelove.jpg";
+import Music2 from "../../assets/money-album.jpg";
+import Music3 from "../../assets/my_heart.jpeg";
+
+const tracks = [
+  {
+    id: 1,
+    title: "The girl in lemonade",
+    artist: "Freed ft AbdulKuduz",
+    music_art: Music1,
+    type: "premium",
+    duration: "3:45",
+  },
+  {
+    id: 2,
+    title: "Another Song",
+    artist: "Artist Name",
+    music_art: Music2,
+    type: "standard",
+    duration: "4:20",
+  },
+  {
+    id: 3,
+    title: "Yet Another Song",
+    artist: "Another Artist",
+    music_art: Music3,
+    type: "premium",
+    duration: "5:10",
+  },
+  {
+    id: 4,
+    title: "The girl in lemonade",
+    artist: "Freed ft AbdulKuduz",
+    music_art: Music1,
+    type: "standard",
+    duration: "3:45",
+  },
+  {
+    id: 5,
+    title: "Another Song",
+    artist: "Artist Name",
+    music_art: Music2,
+    type: "standard",
+    duration: "4:20",
+  },
+  {
+    id: 6,
+    title: "Yet Another Song",
+    artist: "Another Artist",
+    music_art: Music3,
+    type: "standard",
+    duration: "5:10",
+  },
+  {
+    id: 7,
+    title: "Another Song",
+    artist: "Artist Name",
+    music_art: Music2,
+    type: "Premium",
+    duration: "4:20",
+  },
+  {
+    id: 8,
+    title: "Yet Another Song",
+    artist: "Another Artist",
+    music_art: Music3,
+    type: "standard",
+    duration: "5:10",
+  },
+];
 
 const Home = () => {
   return (
     <div className={styles.homeContainer}>
-      <div className={styles.heroContainer}>
+      <section className={styles.heroContainer}>
         <div className={styles.left}>
           <h1 className={styles.title}>
             THE MULTI-UNIVERSAL <br /> MUSIC PLAYLIST
@@ -30,7 +101,16 @@ const Home = () => {
             <img src={Player} alt="Music Player" />
           </div>
         </div>
-      </div>
+      </section>
+
+      <section className={styles.newReleases}>
+        <h2 className={styles.sectionTitle}>New Releases</h2>
+        <div className={styles.musicGrid}>
+          {tracks.map((track) => (
+            <MusicCard key={track.id} track={track} />
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
