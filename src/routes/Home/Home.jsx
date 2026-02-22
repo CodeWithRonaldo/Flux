30ad564ba574d2040bd4e8c143869dae81a4bbfb
olamilekan5162 <opeyemiolalekan481@gmail.com>
2026-02-22 23:32:14 +0100

    reponsiveness adjustment

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -12,7 +12,7 @@ import ArtistOnboarding from "../../components/ArtistOnboarding/ArtistOnboarding
 import Subscribe from "../../components/Subscribe/Subscribe";
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   const { currentTrack } = useAudio();
   return (
