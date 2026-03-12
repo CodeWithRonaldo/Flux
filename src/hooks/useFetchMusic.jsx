33cdef7794a14aec2a54a2e2852790ea2c69a419
Abdulkudus777 <Abdulkudusakanji777@gmail.com>
2026-03-12 00:24:56 +0000

    Update UseFetchMusic hook

--- src/hooks/useFetchMusic.jsx
+++ src/hooks/useFetchMusic.jsx
@@ -1,38 +1,54 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
 import { useNetworkVariable } from "../config/networkConfig";
-import { useIotaClientQuery } from "@iota/dapp-kit";
+import { useIotaClient, useIotaClientQuery } from "@iota/dapp-kit";
+import { useQuery } from "@tanstack/react-query";
 
 export const useFetchMusic = () => {
-  const [musics, setMusics] = useState([]);
   const vibeTraxPackageId = useNetworkVariable("vibeTraxPackageId");
+  const client = useIotaClient();
 
-  const {
-    data: musicData,
-    isPending,
-    isError,
-  } = useIotaClientQuery(
+  const { data: musicIds, isPending: isEventsPending, isError } = useIotaClientQuery(
     "queryEvents",
-    {
-      query: {
-        MoveEventType: `${vibeTraxPackageId}::vibetrax::MusicUploaded`,
-      },
-    },
-    {
-      select: (data) => data.data.flatMap((x) => x.parsedJson),
-    },
+    { query: { MoveEventType: `${vibeTraxPackageId}::vibetrax::MusicUploaded` } },
+    { select: (data) => data.data.map((x) => x.parsedJson.music_id) }
   );
 
-  useEffect(() => {
-    if (!isPending) {
-      setMusics(musicData);
-    }
-  }, [isPending]);
+  const { data: musics, isPending: isObjectsPending } = useQuery({
+    queryKey: ["musicObjects", musicIds],
+    enabled: !!musicIds && musicIds.length > 0,
+    queryFn: async () => {
+      const objects = await client.multiGetObjects({
+        ids: musicIds,
+        options: { showContent: true },
+      });
+      return objects
+        .filter((obj) => obj.data?.content?.fields)
+        .map((obj) => {
+          const f = obj.data.content.fields;
+          return {
+            music_id: obj.data.objectId,
+            title: f.title,
+            description: f.description,
+            genre: f.genre,
+            music_image: f.music_image,
+            preview_music: f.preview_music,
+            full_music: f.full_music,
+            price: f.price,
+            for_sale: f.for_sale,
+            likes: f.likes,
+            streaming_count: f.streaming_count,
+            boost_expiry_ms: f.boost_expiry_ms,
+            creation_time: f.creation_time,
+            artist: f.artist?.fields ?? f.artist,
+            current_owner: f.current_owner?.fields ?? f.current_owner,
+            collaborators: (f.collaborators ?? []).map((c) => c.fields ?? c),
+          };
+        });
+    },
+  });
 
   return {
-    musics,
-    isPending,
+    musics: musics ?? [],
+    isPending: isEventsPending || isObjectsPending,
     isError,
   };
 };
